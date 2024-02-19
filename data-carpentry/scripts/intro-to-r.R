e5444972f045f1a8946f40cd0c8bfde766d3fedf
Kyri Janssen <kmjjanssen@tudelft.nl>
2024-02-19 10:21:23 +0100

    gitautosync update 36

--- data-carpentry/scripts/intro-to-r.R
+++ data-carpentry/scripts/intro-to-r.R
@@ -21,4 +21,4 @@ y <- sqrt(x)
 numeric_vector <- c(2, 6, 3)
 str(numeric_vector)
 
-character_vector <- c('Amsterdam', 'Rotterdam',   )
+character_vector <- c('Amsterdam', 'Rotterdam', ''s-Gravenhage'  )
