730c919e8ec4f3262cb4c3cb82f307707a3e999b
dcannatella <daniele.cannatella@gmail.com>
2024-02-20 11:16:48 +0100

    gitautosync update 119

--- data-carpentry/scripts/raster.R
+++ data-carpentry/scripts/raster.R
@@ -109,7 +109,7 @@ ggplot() +
 
 # Dealing with different CRS
 
-DTM_TUD <- rast("data/tud-dtm-5m-hill.tif")
+DTM_TUD <- rast("data/tud-dtm-5m.tif")
 DTM_hill_TUD <- rast("data/tud-dtm-5m-hill-WGS84.tif")
 
 DTM_TUD_df <- as.data.frame(DTM_TUD, xy = TRUE)
