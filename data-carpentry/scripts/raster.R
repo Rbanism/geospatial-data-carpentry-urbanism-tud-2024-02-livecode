24bc7263643107ca7f69e07f84c63318653f94d7
dcannatella <daniele.cannatella@gmail.com>
2024-02-20 11:13:22 +0100

    gitautosync update 116

--- data-carpentry/scripts/raster.R
+++ data-carpentry/scripts/raster.R
@@ -119,4 +119,6 @@ ggplot() +
   geom_raster(data = DTM_TUD_df,
               aes(x = x, y = y,
                   fill = `tud-dtm-5m`)) +
-  geom_raster(data = DTM_hill_TUD_df)
+  geom_raster(data = DTM_hill_TUD_df,
+              aes(x = x, y = y,
+                  ))
