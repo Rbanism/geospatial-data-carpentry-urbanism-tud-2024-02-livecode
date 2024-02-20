9293391c65f4667850dd6661acb6ed3d1556543f
dcannatella <daniele.cannatella@gmail.com>
2024-02-20 12:03:25 +0100

    gitautosync update 176

--- data-carpentry/scripts/raster.R
+++ data-carpentry/scripts/raster.R
@@ -183,3 +183,7 @@ RGB_band1_TUD <- rast("data/tudlib-rgb.tif", lyrs = 1)
 
 RGB_band1_TUD_df <- as.data.frame(RGB_band1_TUD, xy = TRUE)
 
+ggplot() +
+  geom_raster(data = RGB_band1_TUD_df,
+              aes(x = x, y = y,
+                `tudlib-rgb_1`  alpha = ))
