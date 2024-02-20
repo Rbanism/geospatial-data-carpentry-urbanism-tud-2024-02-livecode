1e457b919894c1466b2d5cb5a2a6c28411412624
dcannatella <daniele.cannatella@gmail.com>
2024-02-20 10:50:43 +0100

    gitautosync update 96

--- data-carpentry/scripts/raster.R
+++ data-carpentry/scripts/raster.R
@@ -103,5 +103,6 @@ ggplot() +
 
 ggplot() +
   geom_raster(data = DSM_TUD_df, aes(x=x, y=y, fill=fct_elevation_cb)) +
-  scale_fill_manual(values = terrain.colors(3))
+  scale_fill_manual(values = terrain.colors(4)) +
+  coord_quickmap()
   
