6700aa5a04842477380e4300c339ebd6990e3dd5
dcannatella <daniele.cannatella@gmail.com>
2024-02-20 09:47:08 +0100

    gitautosync update 36

--- data-carpentry/scripts/raster.R
+++ data-carpentry/scripts/raster.R
@@ -38,5 +38,6 @@ str(DSM_TUD_df)
 
 ggplot() +
   geom_raster(data = DSM_TUD_df, aes(x=x, y=y, fill=tud-dsm-5m )) +
-  scale_fill_viridis_c(option = "H")
+  scale_fill_viridis_c(option = "H") + #contrasting color scale
+  coord_quickmap()
   
