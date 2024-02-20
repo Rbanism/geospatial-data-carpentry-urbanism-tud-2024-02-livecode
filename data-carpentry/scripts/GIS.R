2b08a5d79fa2276f8b6748587a12c5c70b724d4a
clementinecttn <clementine.cottineau@iscpif.fr>
2024-02-20 14:09:54 +0100

    gitautosync update 65

--- data-carpentry/scripts/GIS.R
+++ data-carpentry/scripts/GIS.R
@@ -23,7 +23,7 @@ buildings_polygons <- buildings$osm_polygons %>%
   st_transform(.,crs=28992)
 
 ggplot(data = buildings_polygons) +
-  geom_sf(aes(fill = start_date))
+  geom_sf(aes(fill = start_date),)
 
 
 
