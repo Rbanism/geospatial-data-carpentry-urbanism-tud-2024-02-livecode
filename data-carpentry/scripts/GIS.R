f0ce6a174cbfd1c3e965b12f65a173ad3c3b3a2d
clementinecttn <clementine.cottineau@iscpif.fr>
2024-02-20 14:18:11 +0100

    gitautosync update 77

--- data-carpentry/scripts/GIS.R
+++ data-carpentry/scripts/GIS.R
@@ -27,7 +27,8 @@ str(buildings_polygons)
 buildings_polygons$start_date <- as.numeric(
   buildings_polygons$start_date)
 buildings_polygons$new_date <- 
-  if_else(start_date < 1900, )
+  if_else(start_date < 1900, 1900, 
+          buildings_polygons$start_date)
 
 
 ggplot(data = buildings_polygons) +
