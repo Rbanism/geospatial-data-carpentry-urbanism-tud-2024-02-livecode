0b8d64ea1e25c5098ed3505f07b3ec472006b767
clementinecttn <clementine.cottineau@iscpif.fr>
2024-02-20 16:30:52 +0100

    gitautosync update 250

--- data-carpentry/scripts/GIS.R
+++ data-carpentry/scripts/GIS.R
@@ -130,6 +130,7 @@ ggplot() +
   geom_sf(data = conservation_zones,
           aes(fill = n_old_buildings), 
           colour = NA) +
-  scale_fill_viridis_c(option = "")
+  scale_fill_viridis_c(
+    ,option = "B")
 
 
